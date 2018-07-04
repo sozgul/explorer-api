fa087c3ebd2c7491178c3942c7edda70ffaabca8
Serhan Ozgul <serhanozgul@gmail.com>
2018-07-04 16:56:01 +0200

    initiating dynamoDB admin console

--- server/api.js
+++ server/api.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const path = require('path');
 const api = express();
 
-api.get('/', (req, res) => {
-  res.status(200).send('ExplorerAPI is running');
+
+api.post('/users', (req, res) => {
+  console.log('ExplorerAPI is received posty request to /users route');
+  res.status(200).send('ExplorerAPI is received posty request to /users route');
 });
+
+api.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// api.get('/', (req, res) => {
+//   res.status(200).send('ExplorerAPI is running');
+// });
 
 module.exports = api;
